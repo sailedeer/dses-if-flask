39c24cee51d0c1e7e98e92a35c9c933ce75f667c
Eli Reed <erobreed@uw.edu>
2023-06-30 21:20:42 -0700

    Add todos

--- webserver/static/update_telemetry.js
+++ webserver/static/update_telemetry.js
@@ -1,12 +1,11 @@
 function get_telemetry_data()
 {
+    // Issue #4: use real-time mqtt data
     return "Elevation: 125, Azimuth: 0";
 };
 
 window.onload = function() {
-    //using the function
     telemetry_label = document.getElementsByClassName("telemetry")[0];
-    console.log(telemetry_label);
     telemetry_label.textContent = `${get_telemetry_data()}`
 }
   
